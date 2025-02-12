c57c14cfc7323c86c44f7884c1d2d657f48539b3
ryzhikonion <dvlbbc2@gmail.com>
2025-02-12 20:19:33 +0300

    отформатировал

--- src/s21_convert.c
+++ src/s21_convert.c
@@ -1,60 +1,67 @@
-#include "s21_decimal.h"
 #include <math.h>
+
+#include "s21_decimal.h"
 
 /*
 Я абсолютно не уверен, когда-нибудь проверим
 */
 int s21_from_float_to_decimal(float src, s21_decimal *dst) {
-    int result = 0;
-    if (src == INFINITY) { result = 1; }
-    else if (src == -INFINITY) { result = 2; }
-    else if (src != src) { result = 3; }
-    else {
-        int *bit = (int*)&src;
-        int sign = *bit >> 31 & 1;
-        int scale = (*bit >> 23) - 127;
-        int mantissa = (scale == -127) ? 
-                (*bit && 0x7FFFFF) : (*bit && 0x7FFFFF) | 0x1000000;
-        for (; mantissa & 1 == 0; mantissa >>= 1) {}
-        s21_from_int_to_decimal(mantissa * (sign ? -1 : 1), dst);
-        if (src != (int)src) {
-            s21_decimal sc;
-            s21_from_int_to_decimal((int)pow(2, scale), &sc);
-            s21_div(*dst, sc, dst);
-        }
+  int result = 0;
+  if (src == INFINITY) {
+    result = 1;
+  } else if (src == -INFINITY) {
+    result = 2;
+  } else if (src != src) {
+    result = 3;
+  } else {
+    int *bit = (int *)&src;
+    int sign = *bit >> 31 & 1;
+    int scale = (*bit >> 23) - 127;
+    int mantissa =
+        (scale == -127) ? (*bit && 0x7FFFFF) : (*bit && 0x7FFFFF) | 0x1000000;
+    for (; mantissa & 1 == 0; mantissa >>= 1) {
     }
-    return result;
+    s21_from_int_to_decimal(mantissa * (sign ? -1 : 1), dst);
+    if (src != (int)src) {
+      s21_decimal sc;
+      s21_from_int_to_decimal((int)pow(2, scale), &sc);
+      s21_div(*dst, sc, dst);
+    }
+  }
+  return result;
 }
 
 int s21_from_decimal_to_int(s21_decimal src, int *dst) {
-    int result = 0;
-    s21_decimal min, max;
-    s21_from_int_to_decimal(__INT_MAX__, &max);
-    s21_from_int_to_decimal(-__INT_MAX__, &min);
-    if (s21_is_greater(src, max) || s21_is_less(src, min)) { result = 1; }
-    else {
-        *dst = (int)(pow(10, src.bits[3] >> 16 & 0x00FF) * src.bits[2]);
-    }
-    return result;
+  int result = 0;
+  s21_decimal min, max;
+  s21_from_int_to_decimal(__INT_MAX__, &max);
+  s21_from_int_to_decimal(-__INT_MAX__, &min);
+  if (s21_is_greater(src, max) || s21_is_less(src, min)) {
+    result = 1;
+  } else {
+    *dst = (int)(pow(10, src.bits[3] >> 16 & 0x00FF) * src.bits[2]);
+  }
+  return result;
 }
 
 int s21_from_decimal_to_float(s21_decimal src, float *dst) {
-    int result = 0;
-    *dst = (src.bits[2] / pow(10, src.bits[3] >> 16 & 0x00FF));
-    return result;
+  int result = 0;
+  *dst = (src.bits[2] / pow(10, src.bits[3] >> 16 & 0x00FF));
+  return result;
 }
 
-// Добавить проверку, что конвертация прошла успешно и в зависимости от этого делать код возврата
+// Добавить проверку, что конвертация прошла успешно и в зависимости от этого
+// делать код возврата
 int s21_from_int_to_decimal(int src, s21_decimal *dst) {
-    memset(dst, 0, sizeof(s21_decimal)); // Обнуляем все биты
+  memset(dst, 0, sizeof(s21_decimal));  // Обнуляем все биты
 
-    if (src < 0) {
-        set_sign(dst, 1);
-        src = -src;
-    }
+  if (src < 0) {
+    set_sign(dst, 1);
+    src = -src;
+  }
 
-    dst->bits[0] = src; // Младшие 32 бита
-    set_scale(dst, 0); // Без дробной части
+  dst->bits[0] = src;  // Младшие 32 бита
+  set_scale(dst, 0);   // Без дробной части
 
-    return 0;
+  return 0;
 }
--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -4,9 +4,8 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct 
-{
-    unsigned int bits[4];
+typedef struct {
+  unsigned int bits[4];
 } s21_decimal;
 
 int get_sign(s21_decimal d);
@@ -27,12 +26,12 @@ int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
 int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
 int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
 
-int s21_is_less(s21_decimal, s21_decimal); // <
-int s21_is_less_or_equal(s21_decimal, s21_decimal); // <=
-int s21_is_greater(s21_decimal, s21_decimal); // >
-int s21_is_greater_or_equal(s21_decimal, s21_decimal); // >=
-int s21_is_equal(s21_decimal, s21_decimal); // ==
-int s21_is_not_equal(s21_decimal, s21_decimal); // !=
+int s21_is_less(s21_decimal, s21_decimal);              // <
+int s21_is_less_or_equal(s21_decimal, s21_decimal);     // <=
+int s21_is_greater(s21_decimal, s21_decimal);           // >
+int s21_is_greater_or_equal(s21_decimal, s21_decimal);  // >=
+int s21_is_equal(s21_decimal, s21_decimal);             // ==
+int s21_is_not_equal(s21_decimal, s21_decimal);         // !=
 
 int s21_from_int_to_decimal(int src, s21_decimal *dst);
 int s21_from_float_to_decimal(float src, s21_decimal *dst);
