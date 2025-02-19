8367e38f7c64a508ea6dc7fb1d2f78fc2654bbfa
ryzhikonion <dvlbbc2@gmail.com>
2025-02-19 21:23:17 +0300

    поменял объявления изменённых функций. пока не оттестировал тут будет небольшой беспорядок

--- src/utils.h
+++ src/utils.h
@@ -3,8 +3,23 @@
 
 #include "s21_decimal.h"
 
-int s21_mul_by_10(s21_decimal value, s21_decimal* result);
-int equalize_scales(s21_decimal* value_1, s21_decimal* value_2);
-int equalize(s21_decimal* value, int* scale_to_equalize, int target_scale);
+// int
+// void mul_by_10(s21_decimal value, s21_decimal* result);
+void mul_by_10(s21_decimal* value);
 
+// void div_by_10(s21_decimal value, s21_decimal* result);
+void div_by_10(s21_decimal* value);
+
+int equalize_scales(s21_decimal* value_1, s21_decimal* value_2);
+
+// int equalize(s21_decimal* value, int* scale_to_equalize, int target_scale);
+int equalize(s21_decimal* value_with_smaller_scale,
+             s21_decimal* value_with_larger_scale, int smaller_scale,
+             int larger_scale);
+void invert_result(int* result);
+int is_negative(s21_decimal value);
+void bankers_rounding(s21_decimal* value, unsigned int remainder);
+void round_up(s21_decimal* value);
+int is_next_mul_will_cause_an_overflow(s21_decimal value);
+// int get_scale(s21_decimal value);
 #endif
