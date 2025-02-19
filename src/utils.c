5cddbd0d143e9a9ea901b36354a0aee2d85b0b86
ryzhikonion <dvlbbc2@gmail.com>
2025-02-19 21:18:58 +0300

    добавил реализацию выравнивания масштабов чисел в ситуациях с риском переполнения. нуждается в тестировании

--- src/utils.c
+++ src/utils.c
@@ -1,59 +1,162 @@
 #include "utils.h"
 
-int s21_mul_by_10(s21_decimal value, s21_decimal* result) {
-  int is_overflowed = 0;
+int equalize_scales(s21_decimal* value_1, s21_decimal* value_2) {
+  int all_good = 1;
+  int scale_1 = get_scale(value_1);
+  int scale_2 = get_scale(value_2);
+
+  if (scale_1 < scale_2) {
+    all_good = equalize(value_1, value_2, scale_1, scale_2);
+  } else if (scale_2 < scale_1) {
+    all_good = equalize(value_2, value_1, scale_2, scale_1);
+  }
+
+  return all_good;
+}
+
+int equalize(s21_decimal* value_with_smaller_scale,
+             s21_decimal* value_with_larger_scale, int smaller_scale,
+             int larger_scale) {
+  int all_good = 1;
+  // s21_decimal temp;
+
+  while ((smaller_scale != larger_scale) &&
+         !is_next_mul_will_cause_an_overflow(*value_with_smaller_scale)) {
+    mul_by_10(value_with_smaller_scale);
+    ++(smaller_scale);
+  }
+
+  while (smaller_scale != larger_scale) {
+    div_by_10(value_with_larger_scale);
+    --larger_scale;
+  }
+  return all_good;
+}
+
+int is_next_mul_will_cause_an_overflow(s21_decimal value) {
+  return value.bits[2] > 0x19999999;
+}
+
+// ЗАКОММЕНТИЛ, ПОКА ТЕСТИРУЮ ФУНКЦИЮ БЕЗ ЗАЩИТ
+// int
+// void mul_by_10(s21_decimal value, s21_decimal* result) {
+//   // int is_overflowed = 0;
+//   unsigned long long temp = 0;
+//   unsigned int carry = 0;
+//   s21_decimal res;
+
+//   temp = (unsigned long long)value.bits[0] * 10;
+//   res.bits[0] = (unsigned int)(temp & 0xFFFFFFFF);  // применяем маску, чтобы
+//                                                     // извлечь младшие 32
+//                                                     бита
+//   carry = (unsigned int)(temp >> 32);
+
+//   temp = (unsigned long long)value.bits[1] * 10 + carry;
+//   res.bits[1] = (unsigned int)(temp & 0xFFFFFFFF);
+//   carry = (unsigned int)(temp >> 32);
+
+//   temp = (unsigned long long)value.bits[2] * 10 + carry;
+//   res.bits[2] = (unsigned int)(temp & 0xFFFFFFFF);
+//   carry = (unsigned int)(temp >> 32);
+
+//   // if (carry > 0) {
+//   //   is_overflowed = 1;
+//   // }
+
+//   // if (!is_overflowed) {
+//   res.bits[3] = value.bits[3];
+//   *result = res;
+//   // }
+
+//   // return is_overflowed ? 0 : 1;
+//   // если возвращаем 0, это переполнение
+// }
+
+void mul_by_10(s21_decimal* value) {
+  // int is_overflowed = 0;
   unsigned long long temp = 0;
   unsigned int carry = 0;
   s21_decimal res;
 
-  temp = (unsigned long long)value.bits[0] * 10;
+  temp = (unsigned long long)value->bits[0] * 10;
   res.bits[0] = (unsigned int)(temp & 0xFFFFFFFF);  // применяем маску, чтобы
                                                     // извлечь младшие 32 бита
   carry = (unsigned int)(temp >> 32);
 
-  temp = (unsigned long long)value.bits[1] * 10 + carry;
+  temp = (unsigned long long)value->bits[1] * 10 + carry;
   res.bits[1] = (unsigned int)(temp & 0xFFFFFFFF);
   carry = (unsigned int)(temp >> 32);
 
-  temp = (unsigned long long)value.bits[2] * 10 + carry;
+  temp = (unsigned long long)value->bits[2] * 10 + carry;
   res.bits[2] = (unsigned int)(temp & 0xFFFFFFFF);
   carry = (unsigned int)(temp >> 32);
 
-  if (carry > 0) {
-    is_overflowed += 1;
-  }
+  // if (carry > 0) {
+  //   is_overflowed = 1;
+  // }
 
-  if (!is_overflowed) {
-    res.bits[3] = value.bits[3];
-    *result = res;
-  }
-  return is_overflowed;
-  // если возвращаем 1, это переполнение
+  // if (!is_overflowed) {
+  res.bits[3] = value->bits[3];
+
+  *value = res;
+  set_scale(value, get_scale(value) + 1);
+  // }
+
+  // return is_overflowed ? 0 : 1;
+  // если возвращаем 0, это переполнение
 }
 
-int equalize_scales(s21_decimal* value_1, s21_decimal* value_2) {
-  int all_good = 1;
-  int scale_1 = (value_1->bits[3] >> 16) & 0xFF;
-  int scale_2 = (value_2->bits[3] >> 16) & 0xFF;
+void div_by_10(s21_decimal* value) {
+  s21_decimal temp_result = {{0, 0, 0, 0}};
+  unsigned int remainder = 0;
 
-  if (scale_1 < scale_2) {
-    all_good = equalize(value_1, &scale_1, scale_2);
-  } else if (scale_2 < scale_1) {
-    all_good = equalize(value_2, &scale_2, scale_1);
+  for (int i = 2; i >= 0; --i) {
+    unsigned long long current =
+        ((unsigned long long)remainder << 32) | value->bits[i];
+    temp_result.bits[i] = (unsigned int)current / 10;
+    remainder = (unsigned int)current % 10;
   }
-
-  return all_good;
+  bankers_rounding(&temp_result, remainder);
+  int scale = get_scale(value);
+  if (scale > 0) --scale;
+  temp_result.bits[3] = (value->bits[3] & SIGN_MASK) | scale << 16;
+  *value = temp_result;
 }
 
-int equalize(s21_decimal* value, int* scale_to_equalize, int target_scale) {
-  int all_good = 1;
-  s21_decimal temp;
-  while (*scale_to_equalize != target_scale && all_good) {
-    all_good = s21_mul_by_10(*value, &temp);
-    // если не all_good, то переполнение
+// ВЕРСИЯ С ПРИСВОЕНИЕМ ВРЕМЕННОЙ ПЕРЕМЕННОЙ РЕЗУЛЬТАТА
+// void div_by_10(s21_decimal value, s21_decimal* result) {
+//   s21_decimal temp_result = {{0, 0, 0, 0}};
+//   unsigned int remainder = 0;
+
+//   for (int i = 2; i >= 0; --i) {
+//     unsigned long long current =
+//         ((unsigned long long)remainder << 32) | value.bits[i];
+//     temp_result.bits[i] = (unsigned int)current / 10;
+//     remainder = (unsigned int)current % 10;
+//   }
+//   bankers_rounding(&temp_result, remainder);
+//   int scale = get_scale(&value);
+//   if (scale > 0) ++scale;
+//   temp_result.bits[3] = (value.bits[3] & SIGN_MASK) | scale << 16;
+//   *result = temp_result;
+// }
+
+void bankers_rounding(s21_decimal* value, unsigned int remainder) {
+  if (remainder > 5) {
+    round_up(value);
+
+  } else if (remainder == 5) {
+    if (value->bits[0] % 2 != 0) {
+      round_up(value);
+    }
   }
-  if (all_good) {
-    *value = temp;
+}
+
+void round_up(s21_decimal* value) {
+  for (int i = 0, need_to_continue = 1; i < 3 && need_to_continue; ++i) {
+    ++value->bits[i];
+    if (value->bits[i] != 0) {
+      need_to_continue = 0;
+    }
   }
-  return all_good;
 }
