13dee01fa69c4fcabfdcd9fa7043d8bcd5f83060
ryzhikonion <dvlbbc2@gmail.com>
2025-02-23 23:47:07 +0300

    поменял пару функций на void за ненадобностью возврата какого-либо значения

--- src/utils.c
+++ src/utils.c
@@ -1,25 +1,19 @@
 #include "utils.h"
 
-int equalize_scales(s21_decimal* value_1, s21_decimal* value_2) {
-  int all_good = 1;
+void equalize_scales(s21_decimal* value_1, s21_decimal* value_2) {
   int scale_1 = get_scale(value_1);
   int scale_2 = get_scale(value_2);
 
   if (scale_1 < scale_2) {
-    all_good = equalize(value_1, value_2, scale_1, scale_2);
+    equalize(value_1, value_2, scale_1, scale_2);
   } else if (scale_2 < scale_1) {
-    all_good = equalize(value_2, value_1, scale_2, scale_1);
+    equalize(value_2, value_1, scale_2, scale_1);
   }
-
-  return all_good;
 }
 
-int equalize(s21_decimal* value_with_smaller_scale,
-             s21_decimal* value_with_larger_scale, int smaller_scale,
-             int larger_scale) {
-  int all_good = 1;
-  // s21_decimal temp;
-
+void equalize(s21_decimal* value_with_smaller_scale,
+              s21_decimal* value_with_larger_scale, int smaller_scale,
+              int larger_scale) {
   while ((smaller_scale != larger_scale) &&
          !is_next_mul_will_cause_an_overflow(*value_with_smaller_scale)) {
     mul_by_10(value_with_smaller_scale);
@@ -30,50 +24,13 @@ int equalize(s21_decimal* value_with_smaller_scale,
     div_by_10(value_with_larger_scale);
     --larger_scale;
   }
-  return all_good;
 }
 
 int is_next_mul_will_cause_an_overflow(s21_decimal value) {
   return value.bits[2] > 0x19999999;
 }
 
-// ЗАКОММЕНТИЛ, ПОКА ТЕСТИРУЮ ФУНКЦИЮ БЕЗ ЗАЩИТ
-// int
-// void mul_by_10(s21_decimal value, s21_decimal* result) {
-//   // int is_overflowed = 0;
-//   unsigned long long temp = 0;
-//   unsigned int carry = 0;
-//   s21_decimal res;
-
-//   temp = (unsigned long long)value.bits[0] * 10;
-//   res.bits[0] = (unsigned int)(temp & 0xFFFFFFFF);  // применяем маску, чтобы
-//                                                     // извлечь младшие 32
-//                                                     бита
-//   carry = (unsigned int)(temp >> 32);
-
-//   temp = (unsigned long long)value.bits[1] * 10 + carry;
-//   res.bits[1] = (unsigned int)(temp & 0xFFFFFFFF);
-//   carry = (unsigned int)(temp >> 32);
-
-//   temp = (unsigned long long)value.bits[2] * 10 + carry;
-//   res.bits[2] = (unsigned int)(temp & 0xFFFFFFFF);
-//   carry = (unsigned int)(temp >> 32);
-
-//   // if (carry > 0) {
-//   //   is_overflowed = 1;
-//   // }
-
-//   // if (!is_overflowed) {
-//   res.bits[3] = value.bits[3];
-//   *result = res;
-//   // }
-
-//   // return is_overflowed ? 0 : 1;
-//   // если возвращаем 0, это переполнение
-// }
-
 void mul_by_10(s21_decimal* value) {
-  // int is_overflowed = 0;
   unsigned long long temp = 0;
   unsigned int carry = 0;
   s21_decimal res;
@@ -91,19 +48,10 @@ void mul_by_10(s21_decimal* value) {
   res.bits[2] = (unsigned int)(temp & 0xFFFFFFFF);
   carry = (unsigned int)(temp >> 32);
 
-  // if (carry > 0) {
-  //   is_overflowed = 1;
-  // }
-
-  // if (!is_overflowed) {
   res.bits[3] = value->bits[3];
 
   *value = res;
   set_scale(value, get_scale(value) + 1);
-  // }
-
-  // return is_overflowed ? 0 : 1;
-  // если возвращаем 0, это переполнение
 }
 
 void div_by_10(s21_decimal* value) {
@@ -122,24 +70,6 @@ void div_by_10(s21_decimal* value) {
   temp_result.bits[3] = (value->bits[3] & SIGN_MASK) | scale << 16;
   *value = temp_result;
 }
-
-// ВЕРСИЯ С ПРИСВОЕНИЕМ ВРЕМЕННОЙ ПЕРЕМЕННОЙ РЕЗУЛЬТАТА
-// void div_by_10(s21_decimal value, s21_decimal* result) {
-//   s21_decimal temp_result = {{0, 0, 0, 0}};
-//   unsigned int remainder = 0;
-
-//   for (int i = 2; i >= 0; --i) {
-//     unsigned long long current =
-//         ((unsigned long long)remainder << 32) | value.bits[i];
-//     temp_result.bits[i] = (unsigned int)current / 10;
-//     remainder = (unsigned int)current % 10;
-//   }
-//   bankers_rounding(&temp_result, remainder);
-//   int scale = get_scale(&value);
-//   if (scale > 0) ++scale;
-//   temp_result.bits[3] = (value.bits[3] & SIGN_MASK) | scale << 16;
-//   *result = temp_result;
-// }
 
 void bankers_rounding(s21_decimal* value, unsigned int remainder) {
   if (remainder > 5) {
