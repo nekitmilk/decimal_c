26d6987c1e04108506f36f0d9769a96775351b87
Lososochek <froggod217@gmail.com>
2025-02-24 18:03:25 +0300

    ladno

--- src/s21_convert.c
+++ src/s21_convert.c
@@ -1,14 +1,15 @@
 #include "s21_decimal.h"
 #include <math.h>
+#include <limits.h>
 
 /*
 Я абсолютно не уверен, когда-нибудь проверим
+
+нужно подумать над округлением
 */
 int s21_from_float_to_decimal(float src, s21_decimal *dst) {
     int result = 0;
-    if (src == INFINITY) { result = 1; }
-    else if (src == -INFINITY) { result = 2; }
-    else if (src != src) { result = 3; }
+    if (src == INFINITY || src == -INFINITY || src != src) { result = 1; }
     else {
         int *bit = (int*)&src;
         int sign = *bit >> 31 & 1;
@@ -29,8 +30,8 @@ int s21_from_float_to_decimal(float src, s21_decimal *dst) {
 int s21_from_decimal_to_int(s21_decimal src, int *dst) {
     int result = 0;
     s21_decimal min, max;
-    s21_from_int_to_decimal(__INT_MAX__, &max);
-    s21_from_int_to_decimal(-__INT_MAX__, &min);
+    s21_from_int_to_decimal(INT_MAX, &max);
+    s21_from_int_to_decimal(INT_MIN, &min);
     if (s21_is_greater(src, max) || s21_is_less(src, min)) { result = 1; }
     else {
         *dst = (int)(pow(10, src.bits[3] >> 16 & 0x00FF) * src.bits[2]);
@@ -40,7 +41,19 @@ int s21_from_decimal_to_int(s21_decimal src, int *dst) {
 
 int s21_from_decimal_to_float(s21_decimal src, float *dst) {
     int result = 0;
-    *dst = (src.bits[2] / pow(10, src.bits[3] >> 16 & 0x00FF));
+    s21_decimal sigma, sigma_negate, zero;
+    s21_from_int_to_decimal(0, &zero);
+    sigma.bits[2] = 1;
+    sigma.bits[3] = 0x001c0000; // наглая ложь, нужно переделавать
+    s21_negate(sigma, &sigma_negate);
+    if (s21_is_greater(src, sigma_negate)
+        && s21_is_less(src, sigma)
+        && !s21_is_equal(src, zero)) {
+        *dst = 0;
+        result = 1;
+    } else {
+        *dst = (src.bits[2] / pow(10, src.bits[3] >> 16 & 0x00FF));
+    }
     return result;
 }
 
